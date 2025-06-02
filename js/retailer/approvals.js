96ecefb68ec17bbb491f606370845ec44000d1f4
ebdet2002 <ebdet2002@users.noreply.github.com>
2025-06-02 15:50:02 -0400

    Updated approvals.js

--- js/retailer/approvals.js
+++ js/retailer/approvals.js
@@ -1,17 +1,15 @@
 try {
                      if (isDiscount) {
--                        await DatabaseService.updateDiscountStatus(item.id, status, rejectionReason);
-+                        await DatabaseService.updateDiscountStatus(
-+                            item.id,
-+                            status,
-+                            rejectionReason
-+                        );
+                        await DatabaseService.updateDiscountStatus(
+                            item.id,
+                            status,
+                            rejectionReason
+                        );
                     } else {
--                        await DatabaseService.updatePriceChangeStatus(item.id, status, rejectionReason);
-+                        await DatabaseService.updatePriceChangeStatus(
-+                            item.id,
-+                            status,
-+                            rejectionReason
-+                        );
+                        await DatabaseService.updatePriceChangeStatus(
+                            item.id,
+                            status,
+                            rejectionReason
+                        );
                     }
 }
